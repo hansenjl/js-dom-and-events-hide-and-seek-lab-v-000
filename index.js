17a61bc42a4a7b747fa5979ba493e5ebdafaffb9
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-08-02 19:00:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+   return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML,10) + n).toString()
+  }
+}
+
+function deepestChild(){
+  const node = document.querySelector('div#grand-node')
+  var j = node.children[0]
+  while (j.children[0]){
+      j = j.children[0]
+    }
+    return j 
+}
